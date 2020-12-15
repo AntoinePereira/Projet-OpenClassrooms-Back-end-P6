101a81fbd1ff7675852b850ecdf4e7fb90b82d28
AntoinePereira <toine_42@hotmail.fr>
2020-12-15 16:36:24 +0100

    fonction like/dislike

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect(connectMongoDB,
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-console.log(connectMongoDB)
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
--- controllers/sauces.js
+++ controllers/sauces.js
@@ -53,4 +53,72 @@ exports.deleteSauce = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
-//exports.likeOrDislikeSauce = 
+exports.likeOrDislikeSauce = (req, res, next) => {
+  Sauce.findOne({_id: req.params.id})
+    .then((sauce) => {
+      if (req.body.like === 1 && !sauce.userLiked.includes(req.body.userId)){
+        Sauce.updateOne({ _id: req.params.id },{ $inc: { likes: +1 }, $push: {userLiked: req.body.userId} })
+          .then(() => res.status(200).json({ message: 'sauce liked' }))
+          .catch(error => res.status(400).json({ error }));
+      }
+      else if (req.body.like === -1 && !sauce.userDisliked.includes(req.body.userId)){
+        Sauce.updateOne({ _id: req.params.id },{ $inc: { dislikes: +1 }, $push: {userDisliked: req.body.userId} })
+          .then(() => res.status(200).json({ message: 'sauce disliked' }))
+          .catch(error => res.status(400).json({ error }));
+      }
+      else if (req.body.like === 0){
+        if (sauce.userLiked.includes(req.body.userId)) {
+          Sauce.updateOne({ _id: req.params.id },{ $inc: { likes: -1 }, $pull: {userLiked: req.body.userId} })
+            .then(() => res.status(200).json({ message: 'likes - 1' }))
+            .catch(error => res.status(400).json({ error }));
+        }
+        if (sauce.userDisliked.includes(req.body.userId)) {
+          Sauce.updateOne({ _id: req.params.id },{ $inc: { dislikes: -1 }, $pull: {userDisliked: req.body.userId} })
+            .then(() => res.status(200).json({ message: 'dislikes - 1' }))
+            .catch(error => res.status(400).json({ error }));
+        }
+      }
+    })
+    .catch(error => res.status(400).json({ error }));
+};
+
+/*
+
+  exports.likeOrDislikeSauce = (req,res,next)=>{
+    const like = JSON.parse(req.body.like);
+    Sauce.findOne({_id : req.params.id})
+    .then(sauce=>{
+        if (like == 1){ // if like = 1 the user like the sauce
+            if (!sauce.usersLiked.includes(req.body.userId)){
+                sauce.usersLiked.push(req.body.userId);
+                sauce.likes++;
+            }
+        }
+        else if (like == -1){  // if like = -1 the user dislike the sauce
+            if (!sauce.usersDisliked.includes(req.body.userId)){
+                sauce.usersDisliked.push(req.body.userId);
+                sauce.dislikes++;
+            }
+        }
+        else if (like == 0){ // if like = 0 the user change his mind 
+            if(sauce.usersLiked.includes(req.body.userId)){
+                sauce.usersLiked.splice(sauce.usersLiked.indexOf(req.body.userId),1);
+                sauce.likes--;
+            }
+            else if(sauce.usersDisliked.includes(req.body.userId)){
+                sauce.usersDisliked.splice(sauce.usersDisliked.indexOf(req.body.userId),1);
+                sauce.dislikes--;
+            }
+        }
+        Sauce.updateOne({_id : sauce._id},{$set:{
+            usersLiked:sauce.usersLiked ,
+            usersDisliked : sauce.usersDisliked ,
+            likes : sauce.likes , 
+            dislikes : sauce.dislikes}
+        })
+        .then(()=> res.status(200).json({ message : 'L`utilisateur a donnée son avis'}))
+        .catch(error => res.status(400).json({error}));
+    })
+    .catch(error => res.status(500).json({error}));
+}
+*/
